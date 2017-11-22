c601a59ef0386486cc84f8e2ae8035a826cedef0
Dickr <1782463637@qq.com>
2017-11-22 17:05:40 +0800

    添加队列（顺序表实现)

---
+++ 队列/队列（顺序表实现）.py
@@ -0,0 +1,59 @@
+from exceptions import Exception
+
+class UnderFlow(Exception):
+    pass
+
+class Queue(object):
+
+    def __init__(self, length=2):
+        self.length = length
+        self.elems = [0] * length
+        self.head = 0
+        self.count = 0
+
+    def is_empty(self):
+        return self.count == 0
+
+    def peek(self):
+        if self.is_empty():
+            raise UnderFlow()
+        return self.elems[self.head]
+
+    def dequeue(self):
+        if self.is_empty():
+            raise UnderFlow()
+        elem = self.elems[self.head]
+        self.head = (self.head + 1) % self.count
+        self.count -= 1
+        return elem
+
+    def expand(self):
+        new_length = self.length * 2
+        list_ = [0] * new_length
+        for i in range(self.length):
+            list_[i] = self.elems[i]
+            # list_[i] = self.elems[(self.head + i) % self.length]
+        self.elems = list_
+        self.head = 0
+        self.length = new_length
+
+    def enqueue(self, elem):
+        if self.count == self.length:
+            self.expand()
+        self.elems[(self.head + self.count) % self.length] = elem
+        self.count += 1
+
+
+q = Queue()
+q.enqueue(1)
+q.enqueue(2)
+print q.elems
+q.dequeue()
+q.enqueue(3)
+q.enqueue(4)
+q.enqueue(5)
+q.dequeue()
+q.enqueue(6)
+print q.elems
+
+
