8eb4a8772e1a38126cabf0e7651418ef73c6f95f
Dickr <1782463637@qq.com>
2017-12-08 16:03:52 +0800

    添加二分查找递归算法

--- 算法题/二分查找.py
+++ 算法题/二分查找.py
@@ -1,5 +1,13 @@
 #coding: utf-8
 
+"""
+最坏时间复杂度	O(log n)
+最优时间复杂度	O(1)
+平均时间复杂度	O(log n)
+空间复杂度 迭代： O(1)  递归： O(log n)
+
+"""
+
 def binary_search(list_, elem):
     length = len(list_)
     left, right = 0, length - 1
@@ -13,5 +21,20 @@ def binary_search(list_, elem):
             right = mid - 1
     return 'not found'
 
-print binary_search([1, 2, 3, 4, 5, 6, 9], 7)
+
+def binary_search_recursively(list_, elem, left, right):
+    if left > right:
+        return 'not found'
+
+    mid = (left + right) // 2
+    if elem == list_[mid]:
+        return mid
+    elif elem > list_[mid]:
+        return binary_search_recursively(list_, elem, mid + 1, right)
+    else:
+        return binary_search_recursively(list_, elem, left, mid - 1)
+
+
+# print binary_search([1, 3, 4, 6, 7, 8, 10, 13, 14], 6)
+print binary_search_recursively([1, 3, 4, 6, 7, 8, 10, 13, 14], 6, 0, 8)
 
