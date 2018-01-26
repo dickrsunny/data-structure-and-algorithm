f91c2f498d38f36b4e0742849eef58ba467b5b63
Dickr <1782463637@qq.com>
2018-01-26 15:02:32 +0800

    删除无用函数

--- 排序/插入排序.py
+++ 排序/插入排序.py
@@ -1,18 +1,6 @@
 #coding: utf-8
+
 def insertSort(A, n):
-    # write code here
-    if not A or n == 1:
-        return A
-
-    for i in range(1, n):
-        for j in range(i):
-            if A[i] < A[j]:
-                temp = A[i]
-                A[i] = A[j]
-                A[j] = temp
-    return A
-
-def insertSort2(A, n):
     if not A or n == 1:
         return A
 
