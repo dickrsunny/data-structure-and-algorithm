45a36c9f7fea5942235dbc03433cda6c95fb8049
Dickr <1782463637@qq.com>
2018-01-26 16:06:45 +0800

    添加鲁棒性代码及注释

--- 排序/选择排序.py
+++ 排序/选择排序.py
@@ -1,6 +1,13 @@
+#coding: utf-8
+
+
 def selectionSort(alist):
+    if not alist or len(alist) == 1:
+        return
+
     for i in range(len(alist) - 1, 0, -1):
         maxone = 0
+        # 每次找到剩余列表中的最大值
         for j in range(1, i + 1):
             if alist[j] > alist[maxone]:
                 maxone = j
@@ -9,4 +16,4 @@ def selectionSort(alist):
         alist[maxone] = temp
     return alist
 
-print(selectionSort([1, 2, 7, 5, 2, 9, 3]))
+print(selectionSort([10, 2, 7, 5, 2, 9, 3]))
