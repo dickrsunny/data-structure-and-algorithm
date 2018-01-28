e29ebe67ac3cad75089ed2446a0e5021c597ee50
Dickr <1782463637@qq.com>
2018-01-28 18:53:36 +0800

    删除注释

--- 排序/插入排序.py
+++ 排序/插入排序.py
@@ -5,14 +5,10 @@ def insertSort(A, n):
         return A
 
     for i in range(1, n):
-        temp = A[i]
         j = i
-        # 找到待插入的位置并后移元素
-        while j > 0 and temp < A[j - 1]:
-            A[j] = A[j - 1]
+        while j > 0 and A[j] < A[j - 1]:
+            A[j], A[j - 1] = A[j - 1], A[j]
             j -= 1
-        # 插入当前元素
-        A[j] = temp
     return A
 
 print insertSort([1, 2, 7, 5, 2, 9, 3], 7)
