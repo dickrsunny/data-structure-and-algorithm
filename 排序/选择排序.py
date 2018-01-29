8298fced548728d56a1055c36b23c7eba21da1c2
Dickr <1782463637@qq.com>
2018-01-29 14:32:12 +0800

    修改选择排序函数

--- 排序/选择排序.py
+++ 排序/选择排序.py
@@ -5,15 +5,15 @@ def selectionSort(alist):
     if not alist or len(alist) == 1:
         return
 
-    for i in range(len(alist) - 1, 0, -1):
-        maxone = 0
-        # 每次找到剩余列表中的最大值
-        for j in range(1, i + 1):
-            if alist[j] > alist[maxone]:
-                maxone = j
-        temp = alist[i]
-        alist[i] = alist[maxone]
-        alist[maxone] = temp
+    length = len(alist)
+    # 依次遍历数组中的元素，在剩余的元素中找比自己小的元素，若找到，则交换值，否则和自身交换
+    for i in xrange(length):
+        min_site = i
+        for j in xrange(i + 1, length):
+            if alist[j] < alist[min_site]:
+                min_site = j
+        alist[i], alist[min_site] = alist[min_site], alist[i]
     return alist
+
 
 print(selectionSort([10, 2, 7, 5, 2, 9, 3]))
