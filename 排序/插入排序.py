56756f20fbac6ebac638b6d865c0364e616d78d5
Dickr <1782463637@qq.com>
2018-01-29 10:30:31 +0800

    替换range为xrange

--- 排序/插入排序.py
+++ 排序/插入排序.py
@@ -4,7 +4,8 @@ def insertSort(A, n):
     if not A or n == 1:
         return A
 
-    for i in range(1, n):
+    # 比较当前数和前一个数的大小，如果前一个数大于当前数，就交换位置
+    for i in xrange(1, n):
         j = i
         while j > 0 and A[j] < A[j - 1]:
             A[j], A[j - 1] = A[j - 1], A[j]
