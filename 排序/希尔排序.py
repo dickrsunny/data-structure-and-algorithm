37505d060b0388a238798b2e103c9b6a91d71794
Dickr <1782463637@qq.com>
2018-01-29 16:53:30 +0800

    修改希尔排序函数

--- 排序/希尔排序.py
+++ 排序/希尔排序.py
@@ -1,13 +1,19 @@
+#coding: utf-8
+
 def shellSort(A, n):
-    gap = int(round(n / 2))
-    while gap > 0:
-        for i in range(gap, n):
-            if A[i - gap] > A[i]:
-                tmp = A[i]
-                A[i] = A[i - gap]
-                A[i - gap] = tmp
-        gap -= 1
+    h = 1
+    while h < n // 3:
+        h = 3 * h + 1
+
+    while h >= 1:
+        for i in xrange(1, n):
+            j = i
+            while j >=h and A[j - h] > A[j]:
+                A[j - h], A[j] = A[j], A[j - h]
+                j -= h
+        h //= 3
     return A
+
 
 
 print shellSort([1, 2, 7, 5, 2, 9, 3], 7)
