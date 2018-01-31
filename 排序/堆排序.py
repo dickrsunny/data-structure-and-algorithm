f479843de8c4f0dabd9759a569b844f7e51986f1
Dickr <1782463637@qq.com>
2018-01-31 15:14:58 +0800

    修改堆排序函数

--- 排序/堆排序.py
+++ 排序/堆排序.py
@@ -1,3 +1,4 @@
+"""
 class HeapSort:
     def heapSort(self, A, n):
         if A is not None and n >= 2:
@@ -35,3 +36,41 @@ class HeapSort:
 
 h = HeapSort()
 print h.heapSort([1, 2, 7, 5, 2, 9, 3], 7)
+"""
+
+class HeapSort(object):
+
+    def sort(self, list_):
+        if not list_ or len(list_) == 1:
+            return list_
+
+        self.heapify(list_)
+        length = len(list_) - 1
+        for i in xrange(length, 0, -1):
+            list_[0], list_[i] = list_[i], list_[0]
+            self.sift_down(list_, 0, i)
+        return list_
+
+    def heapify(self, list_):
+        length = len(list_)
+        half = length // 2 - 1
+        for i in xrange(half, -1, -1):
+            self.sift_down(list_, i, length)
+
+    def sift_down(self, list_, root_pos, length):
+        while root_pos * 2 + 1 < length:
+            left_pos = root_pos * 2 + 1
+            right_pos = root_pos * 2 + 2
+            if right_pos < length and list_[left_pos] < list_[right_pos]:
+                left_pos = right_pos
+
+            if list_[root_pos] < list_[left_pos]:
+                list_[root_pos], list_[left_pos] = list_[left_pos], list_[root_pos]
+            else:
+                break
+
+            root_pos = left_pos
+
+h = HeapSort()
+print h.sort([6, 5, 7, 2, 9, 3])
+
