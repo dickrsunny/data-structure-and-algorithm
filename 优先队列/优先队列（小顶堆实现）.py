a073ec0646ee41621f72952cc727bba5ee2f958e
Dickr <1782463637@qq.com>
2018-01-31 11:48:44 +0800

    修改优先队列函数

--- 优先队列/优先队列（小顶堆实现）.py
+++ 优先队列/优先队列（小顶堆实现）.py
@@ -1,4 +1,5 @@
 #coding: utf-8
+"""
 from exceptions import Exception
 
 
@@ -59,22 +60,24 @@ class PriorityQueue(object):
         last_elem = self.elems[-1]
         self.elems[0] = last_elem
         self.elems.pop()
-        # self.sift_down()
-        self.sift_down_recursively(len(self.elems), 0)
+        self.sift_down(len(self.elems), 0)
+        # self.sift_down_recursively(len(self.elems), 0)
         return first_elem
 
     def sift_down(self, length, root_site):
-        while True:
-            left_child = root_site * 2 + 1
-            right_child = root_site * 2 + 2
-            if left_child < length and self.elems[root_site] > self.elems[left_child]:
-                self.elems[root_site], self.elems[left_child] = self.elems[left_child], self.elems[root_site]
-                root_site = left_child
-            elif right_child < length and self.elems[root_site] > self.elems[right_child]:
-                self.elems[root_site], self.elems[right_child] = self.elems[right_child], self.elems[root_site]
-                root_site = right_child
+        while root_site * 2 + 1 < length:
+            left_site = root_site * 2 + 1
+            right_site = root_site * 2 + 2
+
+            if right_site < length and self.elems[left_site] > self.elems[right_site]:
+                left_site = right_site
+
+            if self.elems[root_site] > self.elems[left_site]:
+                self.elems[root_site], self.elems[left_site] = self.elems[left_site], self.elems[root_site]
             else:
                 break
+
+            root_site = left_site
 
     def sift_down_recursively(self, length, root_site):
         left_child = root_site * 2 + 1
@@ -95,3 +98,71 @@ p = PriorityQueue([1, 2, 7, 5, 2, 9, 3])
 print p.elems
 p.dequeue()
 print p.elems
+"""
+
+
+class PriorityQueue(object):
+
+    def __init__(self, list_=None):
+        if list_ is None:
+            list_ = []
+        self.elems = list_
+        if self.elems:
+            self.heapify()
+
+    @property
+    def is_empty(self):
+        return self.elems == []
+
+    def heapify(self):
+        half = len(self.elems) // 2 - 1
+        for i in xrange(half, -1, -1):
+            self.sift_down(i)
+
+    def sift_down(self, root_site):
+        length = len(self.elems)
+        while 2 * root_site + 1 < length:
+            left_site = 2 * root_site + 1
+            right_site = 2 * root_site + 2
+
+            if right_site < length and self.elems[left_site] < self.elems[right_site]:
+                left_site = right_site
+
+            if self.elems[root_site] < self.elems[left_site]:
+                self.elems[root_site], self.elems[left_site] = self.elems[left_site], self.elems[root_site]
+            else:
+                break
+
+            root_site = left_site
+
+    def dequeue(self):
+        if self.is_empty:
+            raise ValueError
+
+        elem = self.elems[0]
+        self.elems[0], self.elems[-1] = self.elems[-1], self.elems[0]
+        self.elems.pop()
+
+        if len(self.elems) > 1:
+            self.sift_down(0)
+        return elem
+
+    def enqueue(self, val):
+        self.elems.append(val)
+        if len(self.elems) > 1:
+            self.sift_up()
+
+    def sift_up(self):
+        site = len(self.elems) - 1
+        while site > 0 and self.elems[site] > self.elems[(site - 1) // 2]:
+            self.elems[site], self.elems[(site - 1) // 2] = self.elems[(site - 1) // 2], self.elems[site]
+            site = (site - 1) // 2
+
+
+# q = PriorityQueue([6, 5, 7, 2, 9, 3, 1])
+q = PriorityQueue([6, 5, 7, 2, 9, 3])
+print q.elems
+# p.dequeue()
+q.enqueue(10)
+print q.elems
+
