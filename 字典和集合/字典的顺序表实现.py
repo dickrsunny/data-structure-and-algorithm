964ab5d790e9734f2f5bead6fb5f3841ab72a2dc
Dickr <1782463637@qq.com>
2018-02-06 15:06:14 +0800

    添加字典和集合目录

---
+++ 字典和集合/字典的顺序表实现.py
@@ -0,0 +1,80 @@
+#coding: utf-8
+
+
+class Node(object):
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+
+    def __lt__(self, other):
+        return self.key < other.key
+
+    def __gt__(self, other):
+        return self.key > other.key
+
+    def __eq__(self, other):
+        return self.key == other.key
+
+
+class DictList(object):
+    def __init__(self):
+        self.elems = []
+
+    @property
+    def is_empty(self):
+        return self.elems == []
+
+    def binary_search_insert(self, node):
+        low, high = 0, len(self.elems) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if node > self.elems[mid]:
+                low = mid + 1
+            elif node < self.elems[mid]:
+                high = mid - 1
+            else:
+                return mid
+
+        return low
+
+    def insert(self, key, val):
+        node = Node(key, val)
+        if self.is_empty:
+            self.elems.append(node)
+        else:
+            site = self.binary_search_insert(node)
+            self.elems.insert(site, node)
+
+    def binary_search_delete(self, key):
+        low, high = 0, len(self.elems) - 1
+        while low <= high:
+            mid = low + (high - low) // 2
+            if key > self.elems[mid].key:
+                low = mid + 1
+            elif key < self.elems[mid].key:
+                high = mid - 1
+            else:
+                return mid
+
+    def delete(self, key):
+        site = self.binary_search_delete(key)
+        if site:
+            self.elems.pop(site)
+        else:
+            raise ValueError()
+
+
+
+d = DictList()
+d.insert(1, 'q')
+d.insert(3, 'q')
+d.insert(5, 'q')
+d.insert(7, 'q')
+d.insert(2, 'q')
+d.delete(3)
+
+
+for i in d.elems:
+    print i.key, i.val
+
+
