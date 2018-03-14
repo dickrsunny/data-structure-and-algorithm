cf68ac4e833cd8e5febc9f25b3a73b41c74414c5
Dickr <1782463637@qq.com>
2018-03-14 22:39:48 +0800

    添加新解法

--- 算法题/list反转.py
+++ 算法题/list反转.py
@@ -1,4 +1,4 @@
-
+# python2
 class Solution(object):
 
     def list_reverse(self, list_):
@@ -17,6 +17,17 @@ class Solution(object):
             for i in xrange(half):
                 list_[i], list_[-(i + 1)] = list_[-(i + 1)], list_[i]
         return list_
+
+# python3
+class Solution2:
+    def reverse_array(self, array):
+        if not array or len(array) == 1:
+            return array
+
+        length = len(array)
+        for i in range(length // 2):
+            array[i], array[length - 1 - i] = array[length - 1 - i], array[i]
+        return array
 
 
 
