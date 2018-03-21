dd8a2aa0a72bccf30c77a56c44b3d272cc2624cb
Dickr <1782463637@qq.com>
2018-03-21 10:51:50 +0800

    添加新解法

--- 算法题/数字在排序数组中出现的次数.py
+++ 算法题/数字在排序数组中出现的次数.py
@@ -5,7 +5,7 @@
 
 """
 
-
+# python2
 class Solution(object):
     def GetNumberOfK(self, data, k):
         # write code here
@@ -37,6 +37,37 @@ class Solution(object):
             return mid2 - mid1 - 1
         return 0
 
+
+# python3
+class Solution2:
+    def count_number(self, array, target):
+        if not array:
+            return
+
+        length = len(array)
+        low, high = 0, length - 1
+        count = 0
+        while low <= high:
+            mid = low + (high - low) // 2
+            if target > array[mid]:
+                low = mid + 1
+            elif target < array[mid]:
+                high = mid - 1
+            else:
+                count += 1
+                left = right = mid
+                while True:
+                    left -= 1
+                    if left >= 0 and array[left] == target:
+                        count += 1
+                    else:
+                        break
+                while True:
+                    right += 1
+                    if right < length and array[right] == target:
+                        count += 1
+                    else:
+                        return count
 
 
 s = Solution()
