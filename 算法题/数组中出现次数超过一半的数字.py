d77cde820665d303bf74cb1f34a4e10b0e4cf33d
Dickr <1782463637@qq.com>
2018-03-21 15:47:17 +0800

    添加解题思路

--- 算法题/数组中出现次数超过一半的数字.py
+++ 算法题/数组中出现次数超过一半的数字.py
@@ -7,6 +7,16 @@
 
 """
 
+"""
+最优解解题思路：
+如果有符合条件的数字，则它出现的次数比其他所有数字出现的次数和还要多。
+在遍历数组时保存两个值：一是数组中一个数字，一是次数。遍历下一个数字时，
+若它与之前保存的数字相同，则次数加1，否则次数减1；若次数为0，
+则保存下一个数字，并将次数置为1。遍历结束后，所保存的数字即为所求。然后再判断它是否符合条件即可。
+
+
+"""
+
 
 # -*- coding:utf-8 -*-
 class Solution(object):
@@ -38,19 +48,16 @@ class Solution(object):
             return 0
 
         length = len(numbers)
-        if length == 1:
-            return numbers[0]
 
         num, count = numbers[0], 1
-        for i in numbers:
-            if i == num:
+        for i in xrange(1, length):
+            if count == 0:
+                num = numbers[i]
+                count = 1
+            elif num == numbers[i]:
                 count += 1
             else:
                 count -= 1
-
-            if count == 0:
-                num = i
-                count = 1
 
         count = 0
         for j in numbers:
