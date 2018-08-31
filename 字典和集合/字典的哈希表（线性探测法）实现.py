81f106896784e07fb8c83bb78508bc8d1c8fd529
Dickr <1782463637@qq.com>
2018-08-31 18:03:00 +0800

    优化函数

--- 字典和集合/字典的哈希表（线性探测法）实现.py
+++ 字典和集合/字典的哈希表（线性探测法）实现.py
@@ -1,27 +1,33 @@
 # coding: utf-8
 
-class Node(object):
+
+class Node:
     def __init__(self, key, val):
         self.key = key
         self.val = val
 
 
-class Dict(object):
+class Dict:
     def __init__(self):
         self._list = [None] * 4
         # 字典中节点个数
         self.count = 0
 
-    @property
-    def length(self):
+    def __len__(self):
         return len(self._list)
 
-    def __hash(self, key):
-        return hash(key) % self.length
+    def __getitem__(self, key):
+        return self._get(key)
 
-    def resize(self, length):
+    def __setitem__(self, key, val):
+        self._put(key, val)
+
+    def _hash(self, key):
+        return hash(key) % len(self)
+
+    def _resize(self, length):
         new_list = [None] * length
-        for i in xrange(self.length):
+        for i in range(len(self)):
             if isinstance(self._list[i], Node):
                 key, val = self._list[i].key, self._list[i].val
                 m = hash(key) % length
@@ -40,11 +46,13 @@ class Dict(object):
                         m = m % length
         self._list = new_list
 
-    def put(self, key, val):
-        if self.count >= self.length // 2:
-            self.resize(self.length * 2)
+    def _put(self, key, val):
+        if self.count >= len(self) // 2:
+            self._resize(len(self) * 2)
+
         node = Node(key, val)
-        m = self.__hash(key)
+        m = self._hash(key)
+
         while True:
             # 数组当前位置无节点或节点key为None
             if not self._list[m] or self._list[m] == -1:
@@ -57,51 +65,55 @@ class Dict(object):
             # 数组当前位置有节点且键不等于key
             else:
                 m += 1
-                m = m % self.length
+                m = m % len(self)
         self.count += 1
 
-    def get(self, key):
-        m = self.__hash(key)
+    def _get(self, key):
+        m = self._hash(key)
         while self._list[m]:
             if isinstance(self._list[m], Node):
                 if self._list[m].key == key:
                     return self._list[m].val
             m += 1
-            m = m % self.length
+            m = m % len(self)
         raise KeyError(key)
 
-    def _delete(self, key):
-        m = self.__hash(key)
+    def pop(self, key, default=None):
+        m = self._hash(key)
         while self._list[m]:
             if isinstance(self._list[m], Node):
                 if self._list[m].key == key:
                     self._list[m] = -1
                     self.count -= 1
-                    if self.count > 0 and self.count == self.length // 8:
-                        self.resize(self.length // 2)
+                    if self.count > 0 and self.count == len(self) // 8:
+                        self._resize(len(self) // 2)
                     return
             m += 1
-            m = m % self.length
-        raise KeyError(key)
+            m = m % len(self)
+
+        if not default:
+            raise KeyError(key)
+        else:
+            return default
 
 
 d = Dict()
-d.put(4, 0)
-d.put(2, 0)
-d.put(3, 0)
-d.put(1, 0)
-d.put(33, 0)
-d.put(34, 0)
-d.put(35, 1)
-d.put(36, 0)
-d._delete(33)
-d._delete(1)
-d._delete(2)
-d._delete(3)
-d._delete(4)
-d._delete(34)
-# d._delete(35)
-d.put(49, 0)
-# print d.get(35)
+d[4] = 0
+d[2] = 0
+d[3] = 0
+d[1] = 0
 
-print ''
+d[33] = 0
+d[34] = 0
+d[35] = 1
+d[36] = 0
+
+d.pop(33)
+d.pop(1)
+d.pop(2)
+d.pop(3)
+d.pop(4)
+d.pop(34)
+d.pop('rr', None)
+
+print(d[35])
