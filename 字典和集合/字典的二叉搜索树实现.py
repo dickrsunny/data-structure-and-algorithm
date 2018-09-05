2746daa5b49e1fd035831b0a181c3935aab4e76c
Dickr <1782463637@qq.com>
2018-09-05 16:23:24 +0800

    优化实现

--- 字典和集合/字典的二叉搜索树实现.py
+++ 字典和集合/字典的二叉搜索树实现.py
@@ -1,77 +1,109 @@
 #coding: utf-8
 
-class Node(object):
+
+'''
+详情参考：https://github.com/julycoding/The-Art-Of-Programming-By-July/blob/master/ebook/zh/03.01.md
+二叉查找树的删除
+继续讲解之前，补充说明下二叉树结点删除的几种情况，待删除的结点按照儿子的个数可以分为三种：
+
+1.没有儿子，即为叶结点。直接把父结点的对应儿子指针设为NULL，删除儿子结点就OK了。
+2.只有一个儿子。那么把父结点的相应儿子指针指向儿子的独生子，删除儿子结点也OK了。
+3.有两个儿子。这是最麻烦的情况，因为你删除结点之后，还要保证满足搜索二叉树的结构。其实也比较容易，我们可以选择左儿子中的最大元素或者右儿子中的最小元素放到待删除结点的位置，就可以保证结构的不变。当然，你要记得调整子树，毕竟又出现了结点删除。习惯上大家选择左儿子中的最大元素，其实选择右儿子的最小元素也一样，没有任何差别，只是人们习惯从左向右。这里咱们也选择左儿子的最大元素，将它放到待删结点的位置。左儿子的最大元素其实很好找，只要顺着左儿子不断的去搜索右子树就可以了，直到找到一个没有右子树的结点。那就是最大的了
+'''
+
+
+class Node:
     def __init__(self, key, value, left=None, right=None):
         self.key = key
         self.value = value
         self.left = left
         self.right = right
 
+    def __gt__(self, other):
+        return self.key > other.key
 
-class Dict(object):
+    def __lt__(self, other):
+        return self.key < other.key
+
+
+class Dict:
     def __init__(self):
         self.root = None
 
     @property
     def is_empty(self):
-        return self.root == None
+        return self.root is None
 
-    def get(self, key):
+    def __getitem__(self, key):
         if self.is_empty:
-            raise ValueError('dict is empty')
+            raise KeyError(key)
 
-        return self.__get(key, self.root)
+        node = Node(key, key)
+        val = self._get(node, self.root)
+        if not val:
+            raise KeyError(key)
 
-    def __get(self, key, root):
+        return val
+
+    def _get(self, node, root):
         if not root:
             return
 
-        if key > root.key:
-            return self.__get(key, root.right)
-        elif key < root.key:
-            return self.__get(key, root.left)
+        if node > root:
+            return self._get(node, root.right)
+        elif node < root:
+            return self._get(node, root.left)
         else:
             return root.value
 
-    def put(self, key, value):
-        res = self.__put(key, value, self.root)
+    def __setitem__(self, key, value):
+        node = Node(key, value)
         if self.is_empty:
-            self.root = res
+            self.root = node
 
-    def __put(self, key, value, root):
+        self._put(node, self.root)
+
+    def _put(self, node, root):
         if not root:
-            return Node(key, value)
+            return node
 
-        if key > root.key:
-            root.right = self.__put(key, value, root.right)
-        elif key < root.key:
-            root.left = self.__put(key, value, root.left)
+        if node > root:
+            root.right = self._put(node, root.right)
+        elif node < root:
+            root.left = self._put(node, root.left)
         else:
-            root.value = value
+            root.value = node.value
 
         return root
 
-    def delete_min(self):
-        self.__delete_min(self.root)
+    def pop_min(self):
+        self._delete_min(self.root)
 
-    def __delete_min(self, root):
+    def _delete_min(self, root):
         if not root.left:
             return
 
-        root.left = self.__delete_min(root.left)
+        root.left = self._delete_min(root.left)
         return root
 
-    def delete(self, key):
-        self.__delete(key, self.root)
+    def pop(self, key, default=None):
+        if self.is_empty:
+            if not default:
+                raise KeyError(key)
+            else:
+                return default
 
-    def __delete(self, key, root):
+        node = Node(key, key)
+        self._delete(node, self.root)
+
+    def _delete(self, node, root):
         if not root:
             return
 
-        if key > root.key:
-            root.right = self.__delete(key, root.right)
-        elif key < root.key:
-            root.left = self.__delete(key, root.left)
+        if node > root:
+            root.right = self._delete(node, root.right)
+        elif node < root:
+            root.left = self._delete(node, root.left)
         else:
             if not root.left:
                 return root.right
@@ -95,38 +127,38 @@ class Dict(object):
         return root
 
     def traverse(self):
-        self.__traverse(self.root)
+        self._traverse(self.root)
 
-    def __traverse(self, root):
+    def _traverse(self, root):
         if not root:
             return
 
-        self.__traverse(root.left)
-        print root.key, root.value
-        self.__traverse(root.right)
+        self._traverse(root.left)
+        print(root.key, root.value)
+        self._traverse(root.right)
 
 
 d = Dict()
-d.put(5, 'a')
-d.put(3, 'a')
-d.put(2, 'a')
-d.put(1, 'a')
-d.put(2.5, 'a')
-d.put(4, 'a')
-d.put(3.5, 'a')
-d.put(4.5, 'a')
-d.put(7, 'a')
-d.put(6, 'a')
-d.put(8, 'a')
-d.put(7.5, 'a')
-print d.get(8)
-print ''
+d[5] = 'a'
+d[3] = 'a'
+d[2] = 'a'
+d[1] = 'a'
+
+d[2.5] = 'a'
+d[4] = 'a'
+d[3.5] = 'a'
+d[4.5] = 'a'
+
+d[7] = 'a'
+d[6] = 'a'
+d[8] = 'a'
+d[7.5] = 'a'
+d[8] = 'd'
+
+print(d[8])
+print('')
 d.traverse()
-d.delete_min()
-d.delete(3)
-print ''
+d.pop_min()
+d.pop(3)
+print('')
 d.traverse()
-
-
-
-
