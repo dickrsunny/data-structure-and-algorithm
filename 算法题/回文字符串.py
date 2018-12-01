1a92a88190120f7ede29a0410fff1933894e348d
Dickr <1782463637@qq.com>
2018-12-01 23:03:02 +0800

    添加回文字符串问题

---
+++ 算法题/回文字符串.py
@@ -0,0 +1,56 @@
+# coding: utf-8
+
+"""
+https://leetcode.com/problems/palindrome-linked-list/
+题目描述：
+Given a singly linked list, determine if it is a palindrome.
+
+Example 1:
+
+Input: 1->2
+Output: false
+Example 2:
+
+Input: 1->2->2->1
+Output: true
+Follow up:
+Could you do it in O(n) time and O(1) space?
+"""
+
+"""
+解题思路：
+1: Reverse the first half while finding the middle.
+2: Compare the reversed first half with the second half.
+"""
+
+
+class Solution:
+    def isPalindrome(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        if not head or not head.next:
+            return True
+
+        prev = None
+        slow, fast = head, head
+
+        while fast and fast.next:
+            fast = fast.next.next
+            next_ = slow.next
+            slow.next = prev
+            prev = slow
+            slow = next_
+
+        if fast:
+            slow = slow.next
+
+        while slow:
+            if slow.val != prev.val:
+                return False
+
+            slow = slow.next
+            prev = prev.next
+
+        return True
