3732be8ff59170879be621120ad1fb92cfc137eb
Kane <1782463637@qq.com>
2020-03-28 11:00:01 +0800

    优化代码

--- 链表/单链表.py
+++ 链表/单链表.py
@@ -1,6 +1,3 @@
-#coding:utf-8
-from exceptions import ValueError
-
 
 class LinkedListUnderflow(ValueError):
     pass
@@ -23,32 +20,28 @@ class SingleLinkList(object):
         # 私有属性头结点
         self.head = node
 
-
     # is_empty() 链表是否为空
     def is_empty(self):
-        return self.head == None
-
+        return self.head is not None
 
     # length() 链表长度
     def length(self):
         count = 0  # 数目
         # 当前节点
         current = self.head
-        while current != None:
+        while current is not None:
             count += 1
             # 当前节点往后移
             current = current.next
         return count
 
-
     # travel() 遍历整个链表
     def travel(self):
         # 访问的当前节点
         current = self.head
-        while current != None:
-            print current.elem
+        while current is not None:
+            print(current.elem)
             current = current.next
-
 
     # add(item) 链表头部添加元素
     def add(self, item):
@@ -57,7 +50,6 @@ class SingleLinkList(object):
         node.next = self.head
         # 新链表的头结点为新节点
         self.head = node
-
 
     # append(item) 链表尾部添加元素
     def append(self, item):
@@ -68,11 +60,10 @@ class SingleLinkList(object):
         else:
             # 让指针指向最后节点
             current = self.head
-            while current.next != None:
+            while current.next is not None:
                 current = current.next
             # 最后节点的下一个为新添加的node
             current.next = node
-
 
     # insert(index, item) 指定位置（从0开始）添加元素
     def insert(self, index, item):
@@ -98,12 +89,11 @@ class SingleLinkList(object):
             # 上一个节点的下一个节点为新的节点
             prev.next = node
 
-
     # remove(item) 删除节点
     def remove(self, item):
         current = self.head
         prev = None
-        while current != None:
+        while current is not None:
             if current.elem == item:
                 # 找到要删除的节点元素
                 if not prev:
@@ -118,29 +108,27 @@ class SingleLinkList(object):
                 prev = current
                 current = current.next
 
-
     # 删除最后一个元素
     def pop(self):
         if self.is_empty():
             raise LinkedListUnderflow('empty LinkedList')
         current = self.head
-        if current.next == None:
+        if current.next is None:
             elem = current.elem
             self.head = None
             return elem
         else:
-            while current.next.next != None:
+            while current.next.next is not None:
                 current = current.next
             elem = current.next.elem
             current.next = None
             return elem
 
-
     # search(item) 查找节点是否存在
     def search(self, item):
         # 当前节点
         current = self.head
-        while current != None:
+        while current is not None:
             if current.elem == item:
                 # 找到了
                 return True
@@ -148,15 +136,16 @@ class SingleLinkList(object):
                 current = current.next
         return False
 
+    def swapPairs(self):
+        def swap(head):
+            if head is not None and head.next is not None:
+                next = head.next
+                head.next = swap(next.next)
+                next.next = head
+                return next
+            return head
 
-    def swapPairs(self, head):
-        if head != None and head.next != None:
-            next = head.next
-            head.next = self.swapPairs(next.next)
-            next.next = head
-            self.head = next
-            return next
-        return head
+        self.head = swap(self.head)
 
 
 if __name__ == '__main__':
@@ -171,8 +160,8 @@ if __name__ == '__main__':
 
     single_link_list.append(1)
     single_link_list.append(2)
-    # single_link_list.append(3)
-    # single_link_list.append(4)
+    single_link_list.append(3)
+    single_link_list.append(4)
     # single_link_list.append(5)
     # single_link_list.append(6)
     #
@@ -199,6 +188,6 @@ if __name__ == '__main__':
     #
     # print('-----------长度------------')
     # print(single_link_list.length())
-    # single_link_list.swapPairs(single_link_list.head)
-    # single_link_list.travel()
-    print single_link_list.pop()
+    single_link_list.swapPairs()
+    single_link_list.travel()
+    # print single_link_list.pop()
