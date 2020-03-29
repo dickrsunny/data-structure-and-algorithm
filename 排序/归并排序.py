f8400237a22438188a18649d4398a2e93bc19cea
Kane <1782463637@qq.com>
2020-03-29 21:34:37 +0800

    规范化

--- 排序/归并排序.py
+++ 排序/归并排序.py
@@ -1,67 +1,65 @@
-# coding:utf-8
-
 
 def mergeSort(seq):
     if not seq or len(seq) <= 1:
         return seq
-    else:
-        middle = len(seq) // 2
-        left = mergeSort(seq[:middle])
-        right = mergeSort(seq[middle:])
 
-        i = 0  # left 计数
-        j = 0  # right 计数
-        k = 0  # 总计数
+    middle = len(seq) // 2
+    left = mergeSort(seq[:middle])
+    right = mergeSort(seq[middle:])
 
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                seq[k] = left[i]
-                i += 1
-            else:
-                seq[k] = right[j]
-                j += 1
-            k += 1
+    i = 0  # left 计数
+    j = 0  # right 计数
+    k = 0  # 总计数
 
-        if i < j:
-            remain = left
-            r = i
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            seq[k] = left[i]
+            i += 1
         else:
-            remain = right
-            r = j
+            seq[k] = right[j]
+            j += 1
+        k += 1
 
-        length = len(remain)
-        while r < length:
-            seq[k] = remain[r]
-            r += 1
-            k += 1
+    if i < j:
+        remain = left
+        r = i
+    else:
+        remain = right
+        r = j
 
-        return seq
+    length = len(remain)
+    while r < length:
+        seq[k] = remain[r]
+        r += 1
+        k += 1
+
+    return seq
 
 
 def mergeSort2(seq, start, end):
     if start >= end:
         return seq[start: end + 1]
-    else:
-        middle = (start + end + 1) // 2
-        left = mergeSort2(seq, start, middle - 1)
-        right = mergeSort2(seq, middle, end)
 
-        i = 0  # left 计数
-        j = 0  # right 计数
+    middle = (start + end + 1) // 2
+    left = mergeSort2(seq, start, middle - 1)
+    right = mergeSort2(seq, middle, end)
 
-        temp = []
-        while i < len(left) and j < len(right):
-            if left[i] < right[j]:
-                temp.append(left[i])
-                i += 1
-            else:
-                temp.append(right[j])
-                j += 1
+    i = 0  # left 计数
+    j = 0  # right 计数
 
-        temp.extend(left[i:])
-        temp.extend(right[j:])
+    temp = []
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            temp.append(left[i])
+            i += 1
+        else:
+            temp.append(right[j])
+            j += 1
 
-        return temp
+    temp.extend(left[i:])
+    temp.extend(right[j:])
+
+    return temp
 
 
 print(mergeSort([1, 2, 7, 5, 2, 9, 3]))
